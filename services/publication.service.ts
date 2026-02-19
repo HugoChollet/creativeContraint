784e8dde9571b784ddd448029b2a24d02aaaaba4
HugoChollet <tibolescabo@gmail.com>
2026-02-19 18:33:53 +0100

    add getFeed function and set constraint to public when publishing, service

--- services/publication.service.ts
+++ services/publication.service.ts
@@ -1,5 +1,6 @@
 import { MediaPickerResult } from "@/components/specific/pickers/media-pickers";
 import { supabase } from "@/lib/supabase";
+import { Publication } from "@/types/publication";
 
 export const publicationService = {
   async uploadFile(
@@ -68,6 +69,34 @@ export const publicationService = {
     });
 
     if (error) throw error;
+
+    const { error: constraintError } = await supabase
+      .from("constraint_sets")
+      .update({ is_public: true })
+      .eq("id", payload.constraintId);
+
+    if (constraintError) {
+      console.warn("Could not mark constraint as public:", constraintError);
+    }
     return { success: true, data };
+  },
+
+  async getFeed(): Promise<Publication[]> {
+    const { data, error } = await supabase
+      .from("publications")
+      .select(
+        `
+        *,
+        generated_constraints:constraint_set_id (*) 
+      `,
+      ) // On précise l'alias si nécessaire pour matcher le type
+      .order("created_at", { ascending: false });
+
+    if (error) {
+      console.error("Error fetching feed:", error);
+      throw error;
+    }
+
+    return data as Publication[];
   },
 };
