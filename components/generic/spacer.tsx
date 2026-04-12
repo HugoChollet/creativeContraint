27a30fa39a2440abefbc704297305d4680322ac2
HugoChollet <tibolescabo@gmail.com>
2026-04-12 23:05:46 +0200

    refacto spacer to have visual divider as optional parameter

--- components/generic/spacer.tsx
+++ components/generic/spacer.tsx
@@ -1,5 +1,27 @@
+import { useStyles } from "@/hooks/use-styles";
 import { View } from "react-native";
 
-export function Spacer({ height, width }: { height?: number; width?: number }) {
-  return <View style={{ height, width }} />;
+export function Spacer({
+  height,
+  width,
+  divider,
+}: {
+  height?: number;
+  width?: number;
+  divider?: boolean;
+}) {
+  const { colors } = useStyles();
+
+  return (
+    <View style={{ height, width }}>
+      {divider && (
+        <View
+          style={{
+            height: 1,
+            backgroundColor: colors.disable,
+          }}
+        />
+      )}
+    </View>
+  );
 }
