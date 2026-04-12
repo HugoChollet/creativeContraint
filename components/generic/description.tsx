3020471b6a4f8a1e44f6e8ed6b991cd2503b0065
HugoChollet <tibolescabo@gmail.com>
2026-04-12 18:35:48 +0200

    add generic component description

---
+++ components/generic/description.tsx
@@ -0,0 +1,42 @@
+import { useStyles } from "@/hooks/use-styles";
+import { TextInput } from "react-native";
+
+interface DescriptionProps {
+  description: string;
+  setDescription: (text: string) => void;
+  isLoading?: boolean;
+  projectColor?: string;
+  placeholder?: string;
+}
+
+export default function Description({
+  description,
+  setDescription,
+  isLoading,
+  projectColor = "#000000",
+  placeholder,
+}: DescriptionProps) {
+  const { globalStyles, colors } = useStyles();
+  const projectColorSoft = projectColor.replace(/[\d.]+\)$/g, `0.2)`);
+
+  return (
+    <TextInput
+      style={[
+        globalStyles.input,
+        {
+          height: 120,
+          textAlignVertical: "top",
+          paddingTop: 12,
+          borderColor: projectColorSoft,
+        },
+      ]}
+      placeholder={placeholder}
+      placeholderTextColor={colors.placeholder}
+      multiline
+      numberOfLines={4}
+      value={description}
+      onChangeText={setDescription}
+      editable={!isLoading}
+    />
+  );
+}
